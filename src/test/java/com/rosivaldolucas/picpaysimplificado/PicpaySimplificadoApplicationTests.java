0d2ed39eadb96cf98b9c3aa4a715abc67302d71f
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2023-12-21 07:37:16 -0300

    master: Commit inicial do projeto desafio PicPay Simplificado.

---
+++ src/test/java/com/rosivaldolucas/picpaysimplificado/PicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosivaldolucas.picpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
